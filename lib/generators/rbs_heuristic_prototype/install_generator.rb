3c0ea69900e4b22e027f6574d96d0fe06624d759
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-06-18 16:35:25 +0900

    Implement RbsHeuristicPrototype::InstallGenerator

---
+++ lib/generators/rbs_heuristic_prototype/install_generator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails"
+
+module RbsHeuristicPrototype
+  class InstallGenerator < Rails::Generators::Base
+    def create_raketask
+      create_file "lib/tasks/rbs_heuristic_prototype.rake", <<~RUBY
+        begin
+          require 'rbs_heuristic_prototype/rake_task'
+          RbsHeuristicPrototype::RakeTask.new
+        rescue LoadError
+          # failed to load rbs_heuristic_prototype. Skip to load rbs_heuristic_prototype tasks.
+        end
+      RUBY
+    end
+  end
+end
