342218f92acc52e74bac57abecb2b7f5ae05a178
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-06-18 18:07:32 +0900

    filters should be loaded automatically

--- lib/rbs_heuristic_prototype.rb
+++ lib/rbs_heuristic_prototype.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "rbs_heuristic_prototype/filters/boolean_methods_filter"
+require_relative "rbs_heuristic_prototype/rake_task"
 require_relative "rbs_heuristic_prototype/version"
 
 module RbsHeuristicPrototype
